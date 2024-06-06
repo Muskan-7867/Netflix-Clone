34a008950c4ac202af4be75e96bb8a215ecd8180
muskan <muskanloach984@gmail.com>
2024-06-07 00:11:10 +0530

    add dispatch action

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -2,12 +2,16 @@ import React, { useEffect } from 'react'
 import Header from './Header';
 import { json } from 'react-router-dom';
 import { API_options } from '../../utills/constants';
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovies } from '../../utills/movieslice';
 
 export const Browse = () => {
+  const dispatch = useDispatch()
   const getNowPlayingMovies = async  () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_options)
     const json =  await data.json();
-    console.log(json)
+    console.log(json.results)
+    dispatch(addNowPlayingMovies(json.results))
   }
 
   useEffect(() => {
