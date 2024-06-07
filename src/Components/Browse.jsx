90e5b554c782dc57661abc2287368b98805502d0
muskan <muskanloach984@gmail.com>
2024-06-07 16:27:21 +0530

    reduce some heavy code

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,25 +1,17 @@
 import React, { useEffect } from 'react'
 import Header from './Header';
-import { json } from 'react-router-dom';
-import { API_options } from '../../utills/constants';
-import { useDispatch } from 'react-redux';
-import { addNowPlayingMovies } from '../../utills/movieslice';
+import useNowPLayingMovies from '../hooks/useNowPlayingMovies';
+import Maincontainer from './Maincontainer';
+import Secondarycontainer from './Secondarycontainer';
 
 export const Browse = () => {
-  const dispatch = useDispatch()
-  const getNowPlayingMovies = async  () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_options)
-    const json =  await data.json();
-    console.log(json.results)
-    dispatch(addNowPlayingMovies(json.results))
-  }
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  },[])
+  useNowPLayingMovies()
+  
   return (
     <div>
         <Header />
+        <Maincontainer />
+        <Secondarycontainer />
     </div>
   )
 }
