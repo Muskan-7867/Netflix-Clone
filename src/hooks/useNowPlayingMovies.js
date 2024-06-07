c1c4a46b675e02935f1a159d3b6c3f21e0042016
muskan <muskanloach984@gmail.com>
2024-06-07 16:33:35 +0530

    create new hook

---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,20 @@
+import { API_options } from '../../utills/constants';
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovies } from '../../utills/movieslice';
+import { useEffect } from 'react';
+
+const useNowPLayingMovies = () =>{
+const dispatch = useDispatch()
+  const getNowPlayingMovies = async  () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_options)
+    const json =  await data.json();
+    console.log(json.results)
+    dispatch(addNowPlayingMovies(json.results))
+  }
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  },[])
+}
+
+export default useNowPLayingMovies;
