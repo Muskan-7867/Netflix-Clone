f9b960258e4c5eb713641b076ba8a2387895d597
muskan <muskanloach984@gmail.com>
2024-06-12 22:40:43 +0530

    got video details

--- src/Components/VideoTitle.jsx
+++ src/Components/VideoTitle.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const VideoTitle = ({title,overview}) => {
   return (
-    <div className='px-12 pt-36'>
+    <div className=' w-screen aspect-video px-24 pt-[20%] absolute text-white bg-gradient-to-r from-black'>
 <h1 className='font-bold text-6xl '>{title}</h1>
 <p className='py-6 text-lg w-1/4'>{overview}</p>
   <div className=''>
-<button className='bg-gray-500 rounded-lg text-white px-12 p-4 text-xl bg-opacity-50'>
+<button className='bg-white  text-black px-12 p-4 text-xl rounded-lg hover:bg-opacity-80'>
     ▶ Play</button>
-<button className= 'mx-2 bg-gray-500 rounded-lg text-white px-12 p-4 text-xl bg-opacity-50'>More info</button>
+<button className= 'mx-2 bg-white  text-black px-12 p-4 text-xl rounded-lg hover:bg-opacity-80'>More info</button>
     </div>
     </div>
   )
