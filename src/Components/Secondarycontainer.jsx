8290a4ac36c8633a842d839d3e1f7a170c7a8a9d
muskan <muskanloach984@gmail.com>
2024-06-14 15:19:46 +0530

    add popular movies and upcoming movies data to container

--- src/Components/Secondarycontainer.jsx
+++ src/Components/Secondarycontainer.jsx
@@ -5,11 +5,25 @@ import MovieList from "./MovieList";
 const SecondaryContainer = () => {
   const nowPlayingMovies = useSelector((store) => store.movies.nowPlayingMovies);
   console.log(nowPlayingMovies);
+  const PopularMovies = useSelector((store) => store.movies.PopularMovies);
+  const UpComingMovies = useSelector((store) => store.movies.UpComingMovies);
+
   
   return (
-    <div>
+   nowPlayingMovies && (
+    <div className=" bg-black">
+      <div className="-mt-52 pl-12 relative z-20">
       <MovieList title={"Now Playing"} movies={nowPlayingMovies} />
+      <MovieList title={"Trending "} movies={nowPlayingMovies} />
+      <MovieList title={"Popular"} movies={PopularMovies} />
+      <MovieList title={"Up Coming"} movies={UpComingMovies} />
+      <MovieList title={"Horror "} movies={nowPlayingMovies} />
+p Coming
+      </div>
+      
+
     </div>
+    )
   );
 };
 
