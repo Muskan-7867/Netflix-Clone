2270d040d6f8f5f5923fd7b9bd10bb605427499b
muskan <muskanloach984@gmail.com>
2024-06-14 15:22:43 +0530

    call upcoming and popular movies hooks in browser

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -3,9 +3,13 @@ import Header from './Header';
 import useNowPLayingMovies from '../hooks/useNowPlayingMovies.jsx';
 import Maincontainer from './Maincontainer';
 import Secondarycontainer from './Secondarycontainer';
+import usePopularMovies from '../hooks/usePopularMovies.jsx';
+import useUpComingMovies from '../hooks/useUpComingMovies.jsx';
 
 export const Browse = () => {
   useNowPLayingMovies()
+  usePopularMovies()
+  useUpComingMovies()
   
   return (
     <div>
