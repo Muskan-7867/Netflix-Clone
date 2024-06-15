8ef0262bfc13505a1eadd33405687fdbab65e30d
muskan <muskanloach984@gmail.com>
2024-06-15 13:45:47 +0530

    add new reducers

--- utills/appstore.jsx
+++ utills/appstore.jsx
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer  from './userSlice';
 import movieReducer from './movieslice';
+import gptReducer from './Gptslice';
+import configreducer from './configslice'
 const appstore = configureStore({
     reducer : {
     user:userReducer,
-    movies:movieReducer
+    movies:movieReducer,
+    gpt : gptReducer,
+    config:configreducer,
     },
 
 })
