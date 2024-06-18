8f828048bc80200bd3d1c5bd9e9f4fbf83fd279f
muskan <muskanloach984@gmail.com>
2024-06-18 22:25:55 +0530

    add serach reducer

--- utills/appstore.jsx
+++ utills/appstore.jsx
@@ -3,12 +3,14 @@ import userReducer  from './userSlice';
 import movieReducer from './movieslice';
 import gptReducer from './Gptslice';
 import configreducer from './configslice'
+import searchReducer from './searchslice'
 const appstore = configureStore({
     reducer : {
     user:userReducer,
     movies:movieReducer,
     gpt : gptReducer,
     config:configreducer,
+    search:searchReducer,
     },
 
 })
