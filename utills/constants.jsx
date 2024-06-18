449d68b06d3841fea3078b6359500ad8f9907795
muskan <muskanloach984@gmail.com>
2024-06-18 22:27:06 +0530

    add search movie api

--- utills/constants.jsx
+++ utills/constants.jsx
@@ -10,3 +10,4 @@ export const API_options  = {
       };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780/";
+export const Search_movie_url = "https://api.themoviedb.org/3/search/movie?query=";
