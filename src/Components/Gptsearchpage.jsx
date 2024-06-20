360b334e66df8ab0a49fee3cfab5a47eab5edab0
muskan <muskanloach984@gmail.com>
2024-06-20 15:07:47 +0530

    serachpage

--- src/Components/Gptsearchpage.jsx
+++ src/Components/Gptsearchpage.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Gptsearchbar from './Gptsearchbar'
-import Gptsuggestions from './Gptsuggestions'
+
 
 const Gptsearchpage = () => {
   return (
-    <div>
-       <div className="absolute  -z-10 w-full h-full">
-        <img className="h-full w-full object-cover" src='src/assets/netflix.jpg' alt="Netflix" />
-      </div>
+    <>
+    <div className="fixed  -z-10 w-full h-full">
+    {/* <img className="h-screen w-full object-cover" src='src/assets/netflix.jpg' alt="Netflix" /> */}
+  </div>
+    
+      
       <Gptsearchbar />
-      <Gptsuggestions />
-    </div>
+    
+    
+    </>
   )
 }
 
