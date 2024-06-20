bf3e31127f9167685c4bb9798764d71ad6cba32a
muskan <muskanloach984@gmail.com>
2024-06-20 15:09:10 +0530

    movielist

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
-const MovieList = ({ title, movies }) => {
+const MovieList = ({ title, movies, searchedMovie=false }) => {
   // Add a check to ensure movies is not null or undefined
   if (!movies || movies.length === 0) {
     return <div>No movies available</div>;
@@ -11,11 +11,11 @@ const MovieList = ({ title, movies }) => {
 
   return (
     <div className="px-6">
-      <h1 className="md:text-3xl text-lg text-white py-4 ">{title}</h1>
+      <h1 className={`${ searchedMovie ? "text-black" : "text-white"} md:text-3xl text-lg  py-4`}>{title}</h1>
       <div className="flex overflow-x-scroll no-scrollbar">
         <div className="flex">
           {movies?.map((movie) => (
-            <MovieCard key={movie.id} posterPath={movie.poster_path} />
+            <MovieCard key={movie.id}  movieid ={movie.id} posterPath={movie.poster_path} />
           ))}
         </div>
       </div>
